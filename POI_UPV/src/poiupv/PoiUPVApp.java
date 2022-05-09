1e44a5956844ef2c5fbc01317969aae69a5a6a92
Sergio Pardo <sergiorequenense@gmail.com>
2022-05-09 21:09:29 +0200

    09/05/2022
    Sex machinne

--- POI_UPV/src/poiupv/PoiUPVApp.java
+++ POI_UPV/src/poiupv/PoiUPVApp.java
@@ -19,10 +19,10 @@ public class PoiUPVApp extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/Vistas/PruebaProblemasMapa.fxml"));
         
         Scene scene = new Scene(root);
-        stage.setTitle("Puntos de interes UPV");
+        stage.setTitle("Log In");
         stage.setScene(scene);
         stage.show();
     }
